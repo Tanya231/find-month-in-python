8a73fc2a892ef2cdccd4f0ab870b2d9f2ed04eec
Tanya Singh <53834227+Tanya231@users.noreply.github.com>
2019-11-01 16:50:40 +0530

    Add files via upload

---
+++ findmonth.py
@@ -0,0 +1,27 @@
+print("\t\tFIND MONTH")
+x = int(input("Enter month number : "))
+if x == 1:
+    print("Jenury")
+elif x == 2:
+    print("Febrary")
+elif x == 3 :
+    print("March ")
+elif x==4 :
+    print("April")
+elif x==5 :
+    print("Mai")
+elif x==6 :
+    print("June")
+elif x==7:
+    print("July")
+elif x==8 :
+    print("August")
+elif x==9 :
+    print("September")
+elif x==10 :
+    print("October")
+elif x==11 :
+    print("Nubember")
+elif x==12 :
+    print("December")
+else:print("Wrong choice ")
